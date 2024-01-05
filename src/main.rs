04d962f03d932aeb102da25407ffa2f2ec272bcf
abechanta <abe.chanta@hotmail.com>
2024-01-05 12:50:55 +0900

    Initialization
    - cargo init
    - cargo add vulkano

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags};
+use vulkano::instance::{Instance, InstanceCreateInfo};
+use vulkano::VulkanLibrary;
+
+#[allow(unused)]
+fn main() {
+    let library = VulkanLibrary::new()
+        .expect("no local Vulkan library/DLL");
+    let instance = Instance::new(library, InstanceCreateInfo::default())
+        .expect("failed to create instance");
+    let physical_device = instance
+        .enumerate_physical_devices()
+        .expect("could not enumerate devices")
+        .next()
+        .expect("no devices available");
+    let queue_family_index = physical_device
+        .queue_family_properties()
+        .iter()
+        .enumerate()
+        .position(|(_queue_family_index, queue_family_properties)| {
+            queue_family_properties.queue_flags.contains(QueueFlags::GRAPHICS)
+        })
+        .expect("couldn't find a graphical queue family") as u32;
+    let (device, mut queues) = Device::new(
+        physical_device,
+        DeviceCreateInfo {
+            queue_create_infos: vec![QueueCreateInfo {
+                queue_family_index,
+                ..Default::default()
+            }],
+            ..Default::default()
+        },
+    )
+        .expect("failed to create device");
+    let queue = queues.next().unwrap();
+}

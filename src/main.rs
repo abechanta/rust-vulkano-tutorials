5dab4a235df0690fa7509b810c02b8ad9420b28b
abechanta <abe.chanta@hotmail.com>
2024-01-05 15:53:51 +0900

    Buffer creation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+use std::sync::Arc;
+use vulkano::buffer::{Buffer, BufferCreateInfo, BufferUsage};
+use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBufferUsage, CopyBufferInfo};
+use vulkano::command_buffer::allocator::{StandardCommandBufferAllocator, StandardCommandBufferAllocatorCreateInfo};
 use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags};
 use vulkano::instance::{Instance, InstanceCreateInfo};
+use vulkano::memory::allocator::{AllocationCreateInfo, MemoryTypeFilter, StandardMemoryAllocator};
+use vulkano::sync::{self, GpuFuture};
 use vulkano::VulkanLibrary;
 
 #[allow(unused)]
@@ -33,4 +39,63 @@ fn main() {
     )
         .expect("failed to create device");
     let queue = queues.next().unwrap();
+
+    //
+    // Example operation
+    //
+    let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
+    let src_content: Vec<i32> = (0..64).collect();
+    let src_buffer = Buffer::from_iter(
+        memory_allocator.clone(),
+        BufferCreateInfo {
+            usage: BufferUsage::TRANSFER_SRC,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::PREFER_DEVICE | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
+            ..Default::default()
+        },
+        src_content,
+    )
+        .unwrap();
+    let dst_content: Vec<i32> = (0..64).map(|_| 0i32).collect();
+    let dst_buffer = Buffer::from_iter(
+        memory_allocator.clone(),
+        BufferCreateInfo {
+            usage: BufferUsage::TRANSFER_DST,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::PREFER_DEVICE | MemoryTypeFilter::HOST_RANDOM_ACCESS,
+            ..Default::default()
+        },
+        dst_content,
+    )
+        .unwrap();
+    let command_buffer_allocator = StandardCommandBufferAllocator::new(
+        device.clone(),
+        StandardCommandBufferAllocatorCreateInfo::default(),
+    );
+    let mut builder = AutoCommandBufferBuilder::primary(
+        &command_buffer_allocator,
+        queue_family_index,
+        CommandBufferUsage::OneTimeSubmit,
+    )
+        .unwrap();
+    builder
+        .copy_buffer(CopyBufferInfo::buffers(src_buffer.clone(), dst_buffer.clone()))
+        .unwrap();
+    let command_buffer = builder.build().unwrap();
+    let future = sync::now(device.clone())
+        .then_execute(queue.clone(), command_buffer)
+        .unwrap()
+        .then_signal_fence_and_flush()
+        .unwrap();
+    future.wait(None).unwrap();
+    {
+        let src_content = src_buffer.read().unwrap();
+        let dst_content = dst_buffer.read().unwrap();
+        assert_eq!(*src_content, *dst_content);
+        println!("Everything succeeded!");
+    }
 }
